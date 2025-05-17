5954aee46a32bf53d2a6248a0aba7bc63cac0ef9
Matheus Couto <matheus.couto.n@gmail.com>
2025-05-16 21:45:35 -0300

    Add files via upload

---
+++ aula_7.html/JavaScript.js
@@ -0,0 +1,21 @@
+//let nome = "Matheus"; // varável mutável
+//const idade = 22; // constante
+//var cidade = "Brasília"; // forma antiga (evite se possível)
+
+//alert("Pop-up na tela");
+//document.write("Escrito direto na página");
+
+let a = 10, b = 3;
+
+document.write("Soma: " + (a + b) + "<br>"); // 13
+document.write("Subtrtação: " + (a - b) + "<br>"); // 7
+document.write("Multiplicação: " + (a * b) + "<br>"); // 30
+document.write("Divisão: " + (a / b) + "<br>"); // 3.333...
+document.write("Resto: " + (a % b) + "<br>"); // 1
+document.write("Potência: " + (a ** b) + "<br>"); // 1000
+
+a += 5; // a agoira é 15
+b++; // b agora é 4
+
+document.write("Novo valor de a: " + a + "<br>");
+document.write("Novo valor de b: " + b + "<br>");
