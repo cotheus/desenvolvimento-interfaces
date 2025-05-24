78bafdab8e4da24de97d953c6de60c7b1d09c0f7
Matheus Couto <matheus.couto.n@gmail.com>
2025-05-23 21:54:18 -0300

    Add files via upload

---
+++ aula_10/JavaScript.js
@@ -0,0 +1,55 @@
+/* let idade = 18;
+
+if (idade >= 18) {
+    console.log("Você é maior de idade.");
+} else {
+    console.log("Você é menor de idade.")
+} */
+
+/* let nota = 7;
+
+if (nota >= 9) {
+    console.log("Excelente");
+} else if (nota >= 6) {
+    console.log("Aprovado");
+} else {
+    console.log("Reprovado");
+} */
+
+/* let i;
+
+for (i = 0; i < 5; i++) {
+    console.log("Repetição número", i);
+} */
+
+/* let contador = 0;
+
+while (contador < 3) {
+    console.log("Valor:", contador);
+    contador++;
+} */
+
+/* let i = 0
+
+do {
+    console.log("Executa pelo menos uma vez:", i);
+    i++;
+} while (i < 1); */
+
+/* for (let i = 0; i < 10; i++) {
+    if (i === 5) break;
+    console.log(i);
+} */
+
+/* for (let i = 0; i < 5; i++) {
+    if (i === 2) continue;
+    console.log(i);
+} */
+
+/* let num = prompt("Digite um número: ");
+
+if (num % 2 === 0){
+    console.log("Par");
+} else {
+    console.log("Ímpar");
+} */
